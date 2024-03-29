4ebb91e315b2c1eac68f0c1391b5a0d67a85a753
Lance <dogmandevices@gmail.com>
2024-03-29 01:17:25 -0500

    assign each square an ID

--- pages/etch-a-sketch/etch-a-sketch.js
+++ pages/etch-a-sketch/etch-a-sketch.js
@@ -1,12 +1,23 @@
 const gridContainer = document.querySelector("#gridContainer");
 
-let squares = [];
 function createGrid() {
+	let squares = [];
 	for (let i = 0; i < 16; i++) {
 		squares[i] = document.createElement("div");
 		squares[i].className = "square";
 		gridContainer.appendChild(squares[i]);
+		let allSquares = document.querySelector("#gridContainer").children;
+		allSquares[i].id = "square" + i;
 	}
-
 }
 createGrid();
+// let square = document.querySelector(".square");
+
+// function draw() {
+	
+// 	square.classList.toggle("drawn");
+// }
+
+// addEventListener("mouseover", (event) => {
+// 	draw();
+// 	});
