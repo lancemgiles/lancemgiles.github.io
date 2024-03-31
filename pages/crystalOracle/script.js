75087c2ffe002b0db716019abea41898fccac2bd
Lance <dogmandevices@gmail.com>
2024-03-31 16:50:22 -0500

    create functions to create answers and write them to the page

--- pages/crystalOracle/script.js
+++ pages/crystalOracle/script.js
@@ -1,7 +1,18 @@
-// yes or no
-let result = "";
-if (Math.floor(Math.random() * 10) % 2) {
-  result = "yes"
-} else {
-  result = "no"
+const theOrb = document.querySelector("#theOrb");
+const answer = document.querySelector("#result");
+
+function getReading() {
+  theOrb.addEventListener("click", () => {
+    answer.innerText = getAnswer();
+  });
+}
+
+function getAnswer() {
+  if ((Math.floor(Math.random() * 10) % 2) === 0) {
+    return "Yes."
+  } else if ((Math.floor(Math.random() * 10) % 3) === 0) {
+    return "Hmm... Ask again later."
+  } else {
+    return "No."
+  }
 }
