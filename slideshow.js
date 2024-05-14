1f3b8083018d6bd22ba4ae6b5bc21a815f27e690
lance <dogmandevices@gmail.com>
2024-05-13 21:26:24 -0500

    add book and album cover slideshow; rewrite js to accomodate multile slideshows; make navbar float over everything

--- slideshow.js
+++ slideshow.js
@@ -1,28 +1,32 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = [1,1];
+let slideId = ["slidesProd", "slidesCov"];
+let dotIds = ["dotP", "dotC"];
+showSlides(1, 0);
+showSlides(1, 1);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n, no);
 }
 
-function showSlides(n) {
+function showSlides(n, no) {
   let i;
-  let slides = document.getElementsByClassName("slides");
-  let dots = document.getElementsByClassName("dot");
+  let x = document.getElementsByClassName(slideId[no]);
+  // let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName(dotIds[no]);
 
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
   
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace("active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  x[slideIndex[no]-1].style.display = "block";
+  dots[slideIndex[no]-1].className += " active";
 }
